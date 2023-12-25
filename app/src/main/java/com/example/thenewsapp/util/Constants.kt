517d24af2ae09a11eb5348cccc82f486c4ac7b27
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-12-25 15:39:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.thenewsapp.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "fd1b489d46d64721a704ed7bc46eb3d8"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
